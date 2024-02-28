fa3c7e972b31259b63528dc3ac41a8e6be408ea0
serizba <serizba@gmail.com>
2024-02-28 09:59:31 +0100

    Use torch.cdist instead of faiss

--- utils/validation.py
+++ utils/validation.py
@@ -1,27 +1,11 @@
 import numpy as np
-import faiss
-import faiss.contrib.torch_utils
-from prettytable import PrettyTable
+import torch
 
 
 def get_validation_recalls(r_list, q_list, k_values, gt, print_results=True, faiss_gpu=False, dataset_name='dataset without name ?', testing=False):
         
-        embed_size = r_list.shape[1]
-        if faiss_gpu:
-            res = faiss.StandardGpuResources()
-            flat_config = faiss.GpuIndexFlatConfig()
-            flat_config.useFloat16 = True
-            flat_config.device = 0
-            faiss_index = faiss.GpuIndexFlatL2(res, embed_size, flat_config)
-        # build index
-        else:
-            faiss_index = faiss.IndexFlatL2(embed_size)
-        
-        # add references
-        faiss_index.add(r_list)
-
-        # search for queries in the index
-        _, predictions = faiss_index.search(q_list, max(k_values))
+        distances = torch.cdist(q_list, r_list, p=2)
+        predictions = torch.topk(distances, k=max(k_values), largest=False, sorted=True).indices.cpu().numpy()
         
         if testing:
             return predictions
@@ -39,10 +23,11 @@ def get_validation_recalls(r_list, q_list, k_values, gt, print_results=True, fai
         d = {k:v for (k,v) in zip(k_values, correct_at_k)}
 
         if print_results:
-            print() # print a new line
-            table = PrettyTable()
-            table.field_names = ['K']+[str(k) for k in k_values]
-            table.add_row(['Recall@K']+ [f'{100*v:.2f}' for v in correct_at_k])
-            print(table.get_string(title=f"Performances on {dataset_name}"))
+            print()
+            print(f"Performances on {dataset_name}")
+            print("Recall@K :", end=" ")
+            print(" | ".join([f"{k:>5}" for k in k_values]))
+            print("           " + " | ".join([f"{100*v:.2f}" for v in correct_at_k])) 
+            print("-"*50)
         
         return d
