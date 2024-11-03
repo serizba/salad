fa551f0e757161bb42d4c714e5eaaa7315d99007
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2024-11-03 22:00:54 +0100

    Make some dependencies optional for inference

--- utils/validation.py
+++ utils/validation.py
@@ -1,11 +1,11 @@
 import numpy as np
-import faiss
-import faiss.contrib.torch_utils
-from prettytable import PrettyTable
 
 
 def get_validation_recalls(r_list, q_list, k_values, gt, print_results=True, faiss_gpu=False, dataset_name='dataset without name ?', testing=False):
-        
+        import faiss
+        import faiss.contrib.torch_utils
+        from prettytable import PrettyTable
+
         embed_size = r_list.shape[1]
         if faiss_gpu:
             res = faiss.StandardGpuResources()
@@ -16,16 +16,16 @@ def get_validation_recalls(r_list, q_list, k_values, gt, print_results=True, fai
         # build index
         else:
             faiss_index = faiss.IndexFlatL2(embed_size)
-        
+
         # add references
         faiss_index.add(r_list)
 
         # search for queries in the index
         _, predictions = faiss_index.search(q_list, max(k_values))
-        
+
         if testing:
             return predictions
-        
+
         # start calculating recall_at_k
         correct_at_k = np.zeros(len(k_values))
         for q_idx, pred in enumerate(predictions):
@@ -34,7 +34,7 @@ def get_validation_recalls(r_list, q_list, k_values, gt, print_results=True, fai
                 if np.any(np.in1d(pred[:n], gt[q_idx])):
                     correct_at_k[i:] += 1
                     break
-        
+
         correct_at_k = correct_at_k / len(predictions)
         d = {k:v for (k,v) in zip(k_values, correct_at_k)}
 
@@ -44,5 +44,5 @@ def get_validation_recalls(r_list, q_list, k_values, gt, print_results=True, fai
             table.field_names = ['K']+[str(k) for k in k_values]
             table.add_row(['Recall@K']+ [f'{100*v:.2f}' for v in correct_at_k])
             print(table.get_string(title=f"Performances on {dataset_name}"))
-        
+
         return d
